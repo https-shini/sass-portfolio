3ce6306d3a69ee4d6982c4cd4acef44a3c903ac4
Guilherme Souza <100307080+https-shini@users.noreply.github.com>
2023-11-15 06:15:57 -0300

    Add files via upload

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,7 +9,7 @@ const Sidebar = () => {
 return (
     <aside id="sidebar">
         <img src={Avatar} alt="Matheus Battisti" />
-        <p className="title">Desenvolvedor</p>
+        <p className="title">Desenvolvedor Full-Stack</p>
         <SocialNetworkContainer />
         <InformationContainer />
         <div>
